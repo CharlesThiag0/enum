74ad6de0316c1aafc085866756d64a117aa2a3c3
Charles <charlesthiagosantos@gmail.com>
2024-04-09 19:04:41 -0300

    feat : basico de enum

---
+++ src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Order order = new Order(1280, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+    }
+}
